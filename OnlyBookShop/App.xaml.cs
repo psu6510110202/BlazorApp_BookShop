bf13f14fc25f0c6f1e55e79ef905dbed294cccf4
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-01-21 00:48:14 +0700

    Initial With Checkpoint 1 About page

---
+++ OnlyBookShop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlyBookShop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ OnlyBookShop/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OnlyBookShop
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
