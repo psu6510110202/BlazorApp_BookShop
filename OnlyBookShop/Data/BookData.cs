9a0e8058e151b5b18cb54e712bb532a12e91d109
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2024-01-23 19:37:48 +0700

    Rework Checkpoint 2

---
+++ OnlyBookShop/Data/BookData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyBookShop.Data
+{
+    public class BookData
+    {
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string Author { get; set; }
+        
+        public int Year { get; set; }
+       
+        public int Price { get; set; }
+        
+        public string Url { get; set; }
+    }
+}
